57fa53a9cc88d836975345e509fb721d244b0058
杨登永 <517697206@qq.com>
2022-03-21 15:07:49 +0800

    feat:redis增加String相关

--- redisexternal/rediscomponent/redigoYouki.go
+++ redisexternal/rediscomponent/redigoYouki.go
@@ -1,6 +1,7 @@
 package rediscomponent
 
 import (
+	"errors"
 	"fmt"
 	"github.com/bingtianyiyan/youki_gotools/redisexternal/redisenum"
 	"github.com/garyburd/redigo/redis"
@@ -80,8 +81,76 @@ func (m *RedisGoYouKi) Execute(methodName string,setExpireTime bool,params ...in
 	}, c
 }
 
+//
+//  ConvertTypeOk
+//  @Description: 部分特殊情况结果转换
+//  @param value
+//  @return int64
+//  @return bool
+//
+func convertTypeOk(value interface{}) (int64,bool){
+	switch value.(type) {
+	case string:
+		// 将interface转为string字符串类型
+		op, ok := value.(string)
+		if ok {
+			if op == "OK"{
+				return 1,ok
+			}
+		}
+		return 0,ok
+	case int32:
+		// 将interface转为int32类型
+		op, ok := value.(int32)
+		return int64(op),ok
+	case int64:
+		// 将interface转为int64类型
+		op, ok := value.(int64)
+		return op,ok
+	default:
+		return 0,false
+	}
+}
 
-//Keys 相关
+func setSingleKeyExpire(cli redis.Conn,key string,expireTimeSecond int)(interface{},error){
+	result,err := cli.Do(redisenum.Key_Expire, key, expireTimeSecond)
+	if err != nil {
+		cli.Do(redisenum.Key_DelKey, key)
+	}
+	return result,err
+}
+
+func setMoreKeysExpire(cli redis.Conn,expireTimeSecond int,params []interface{})(interface{},error){
+	var result interface{}
+	var err error
+	var line = len(params)
+	if line % 2 == 0 {
+		for i := 0; i < line; i++ {
+			if i%2 == 0 {
+				cli.Send(redisenum.Key_Expire,params[i],expireTimeSecond)
+			}
+		}
+		cli.Flush()
+		result,err = cli.Receive()
+		//设置失败则再次删除
+		if err != nil{
+			for i := 0; i < line; i++ {
+				if i%2 == 0 {
+					cli.Send(redisenum.Key_DelKey,params[i])
+				}
+			}
+			cli.Flush()
+			result,err = cli.Receive()
+		}
+	}else {
+		err = errors.New("参数不能为奇数")
+	}
+	return result,err
+}
+
+/*
+Keys 相关
+ */
 
 // KeyExists Key是否存在
 func (m RedisGoYouKi) KeyExists(key string) redisenum.RedisResponse {
@@ -91,3 +160,146 @@ func (m RedisGoYouKi) KeyExists(key string) redisenum.RedisResponse {
 	}
 	return response
 }
+
+// SetExpire 设置过期时间
+func (m RedisGoYouKi) SetExpire(key string,expireTime int) redisenum.RedisResponse {
+	response,_ := m.Execute(redisenum.Key_Expire,false,key,expireTime)
+	return response
+}
+
+// DelKeys 删除Key
+func (m RedisGoYouKi) DelKeys(keys ...interface{}) redisenum.RedisResponse {
+	response,_ := m.Execute(redisenum.Key_DelKey,false,keys...)
+	return response
+}
+
+// Dump 序列化给定 key ，并返回被序列化的值(如果 key 不存在，那么返回 nil 。 否则，返回序列化之后的值
+func (m RedisGoYouKi) Dump(key string) redisenum.RedisResponse {
+	response,_ := m.Execute(redisenum.Key_Dump,false,key)
+	return response
+}
+
+/*
+string 相关
+ */
+
+// Set 设置单个数据 数据格式 key/data/"EX"固定过期时间字符串/expireTime:过期时间1000,如果expireTime==0则不设置过期时间
+func (m RedisGoYouKi) Set(key string,data interface{},expireTime int) redisenum.RedisResponse{
+	if expireTime == 0 {
+		response,_ := m.Execute(redisenum.Str_Set,false,key,data)
+		return response
+	}
+	response,_ := m.Execute(redisenum.Str_Set,false,key,data,"EX",expireTime)
+	return response
+}
+
+// Get 获取单个Set的数据
+func (m RedisGoYouKi) Get(key string)redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_Get,false,key)
+	return response
+}
+
+// GetSet 将给定 key 的值设为 value ，并返回 key 的旧值(old value)
+func (m RedisGoYouKi) GetSet(key string,data interface{})redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_GetSet,false,key,data)
+	return response
+}
+
+// MGet 获取所有(一个或多个)给定 key 的值,返回结果可以用redis 封装的 ByteSlices 直接帮你转换
+func (m RedisGoYouKi) MGet(keys ...interface{})redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_MGet,false,keys...)
+	return response
+}
+
+// SetEx 将值 value 关联到 key ，并将 key 的过期时间设为 seconds (以秒为单位)。
+func (m RedisGoYouKi) SetEx(key string,data interface{},expireTimeSecond int)redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_SetEx,false,key,expireTimeSecond,data)
+	return response
+}
+
+// SetNx 只有在 key 不存在时设置 key 的值。(值存在，设置不成功Data=0，不存在设置成功Data=1
+func (m RedisGoYouKi) SetNx(key string,data interface{},expireTimeSecond int)redisenum.RedisResponse{
+	response,cli := m.Execute(redisenum.Str_SetNx,true,key,data)
+	if cli != nil {
+		defer cli.Close()
+		if  response.Result {
+			var result,ok = convertTypeOk(response.Data)
+			if ok && result == 1 && expireTimeSecond > 0 {
+				response.Data, response.Err = setSingleKeyExpire(cli,key,expireTimeSecond)
+			}
+		}
+	}
+	return response
+}
+
+
+// MSet 同时设置一个或多个 key-value 对。
+func (m RedisGoYouKi) MSet(expireTimeSecond int,params ...interface{}) redisenum.RedisResponse{
+	response,cli := m.Execute(redisenum.Str_MSet,true,params...)
+	if cli != nil {
+		defer cli.Close()
+		if  response.Result {
+			response.Data, response.Err = setMoreKeysExpire(cli,expireTimeSecond,params)
+		}
+	}
+	return response
+}
+
+// MSetNx 同时设置一个或多个 key-value 对，当且仅当所有给定 key 都不存在。(数据存在设置的时候Data返回0，
+func (m RedisGoYouKi) MSetNx(expireTimeSecond int,params ...interface{}) redisenum.RedisResponse{
+	response,cli := m.Execute(redisenum.Str_MSetNx,true,params...)
+	if cli != nil {
+		defer cli.Close()
+		if  response.Result {
+			var result,ok = convertTypeOk(response.Data)
+			if ok && result == 1 {
+				response.Data, response.Err = setMoreKeysExpire(cli, expireTimeSecond, params)
+			}
+		}
+	}
+	return response
+}
+
+// PSetEx 这个命令和 SETEX 命令相似，但它以毫秒为单位设置 key 的生存时间，而不是像 SETEX 命令那样，以秒为单位。
+func (m RedisGoYouKi) PSetEx(key string,data interface{},milliseconds int)redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_PSetEx,false,key,milliseconds,data)
+	return response
+}
+
+// Incr 将 key 中储存的数字值增一
+func (m RedisGoYouKi) Incr(key string) redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_Incr,false,key)
+	return response
+}
+
+// IncrBy 将 key 所储存的值加上给定的增量值（increment） 。
+func (m RedisGoYouKi) IncrBy(key string,num int) redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_Incrby,false,key,num)
+	return response
+}
+
+// IncrByFloat 将 key 所储存的值加上给定的浮点增量值（increment）
+func (m RedisGoYouKi) IncrByFloat(key string,num float64) redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_IncrbyFloat,false,key,num)
+	return response
+}
+
+// Decr 将 key 中储存的数字值减一
+func (m RedisGoYouKi) Decr(key string) redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_Decr,false,key)
+	return response
+}
+
+// DecrBy 将 key 所储存的值加上给定的增量值（increment） 。
+func (m RedisGoYouKi) DecrBy(key string,num int) redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_Decrby,false,key,num)
+	return response
+}
+
+// Append
+//如果 key 已经存在并且是一个字符串， APPEND 命令将指定的 value 追加到该 key 原来值（value）的末尾,返回的result是字符长度
+func (m RedisGoYouKi) Append(key string,data interface{}) redisenum.RedisResponse{
+	response,_ := m.Execute(redisenum.Str_Append,false,key,data)
+	return response
+}
+
--- redisexternal/rediscomponent/redisYouki_test.go
+++ redisexternal/rediscomponent/redisYouki_test.go
@@ -2,6 +2,7 @@ package rediscomponent
 
 import (
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"testing"
 )
 
@@ -21,9 +22,106 @@ func init(){
 	})
 }
 
+/*
+Key相关
+ */
+
 func TestRedisGoYouKi_KeyExists(t *testing.T) {
-   var resp = redisGoCli.KeyExists("key1")
+   var resp = redisGoCli.KeyExists("key")
    if resp.Result{
    	fmt.Println(resp.Result)
    }
+}
+
+func TestRedisGoYouKi_SetExpire(t *testing.T) {
+	var resp = redisGoCli.SetExpire("key",1000000)
+	if resp.Result{
+		fmt.Println(resp.Result)
+	}
+}
+
+func TestRedisGoYouKi_DelKeys(t *testing.T) {
+	var resp = redisGoCli.DelKeys("key")
+	if resp.Result{
+		fmt.Println(resp.Result)
+	}
+}
+
+func TestRedisGoYouKi_Dump(t *testing.T) {
+	var resp = redisGoCli.Dump("key")
+	if resp.Result{
+		str,err := redis.String(resp.Data,resp.Err)
+		fmt.Println(err,str)
+	}
+}
+
+/*
+String相关
+ */
+
+func TestRedisGoYouKi_Set(t *testing.T) {
+	var resp = redisGoCli.Set("key2","234",100000)//设置过期时间
+	var resp2 = redisGoCli.Set("key3","234",0)//不设置过期时间
+	fmt.Println(resp,resp2)
+}
+
+func TestRedisGoYouKi_Get(t *testing.T) {
+	var resp = redisGoCli.Get("key2")
+	fmt.Println(redis.String(resp.Data,resp.Err))
+}
+
+func TestRedisGoYouKi_GetSet(t *testing.T) {
+	var resp = redisGoCli.GetSet("key2","567")
+	fmt.Println(redis.String(resp.Data,resp.Err))
+}
+
+func TestRedisGoYouKi_MGet(t *testing.T) {
+	var resp = redisGoCli.MGet("key2","key3")
+	var byteData,err = redis.ByteSlices(resp.Data,resp.Err)
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	for _,v := range byteData{
+		fmt.Println(string(v))
+	}
+}
+
+func TestRedisGoYouKi_SetEx(t *testing.T) {
+	var resp = redisGoCli.SetEx("key2","999",1000000)
+	fmt.Println(resp)
+}
+
+func TestRedisGoYouKi_SetNx(t *testing.T) {
+	var resp = redisGoCli.SetNx("key2","10000",1000000)
+	fmt.Println(redis.String(resp.Data,resp.Err))
+	var resp2 = redisGoCli.SetNx("key4","10001",1000000)//设置不存在数据
+	fmt.Println(redis.String(resp2.Data,resp2.Err))
+}
+
+func TestRedisGoYouKi_MSet(t *testing.T) {
+	var resp = redisGoCli.MSet(100000,"key05","444","key06","333","key07","你哈M")
+	fmt.Println(redis.String(resp.Data,resp.Err))
+}
+
+func TestRedisGoYouKi_MSetNx(t *testing.T) {
+	var resp = redisGoCli.MSetNx(100000,"key05","444","key06","333")
+	fmt.Println(redis.String(resp.Data,resp.Err))
+	var resp2 = redisGoCli.MSetNx(100000,"key08","444","key09","333")//设置不存在的
+	fmt.Println(redis.String(resp2.Data,resp2.Err))
+}
+
+func TestRedisGoYouKi_PSetEx(t *testing.T) {
+	var resp = redisGoCli.PSetEx("key10","tttt",1000000)
+	fmt.Println(redis.String(resp.Data,resp.Err))
+}
+
+func TestRedisGoYouKi_Incr(t *testing.T) {
+	var resp = redisGoCli.Incr("key4")
+	fmt.Println(redis.String(resp.Data,resp.Err))
+}
+
+func TestRedisGoYouKi_Append(t *testing.T) {
+	var resp = redisGoCli.Append("key09","yyyyy")
+	fmt.Println(redis.Int64(resp.Data,resp.Err))
 }
